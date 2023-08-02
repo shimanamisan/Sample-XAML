7726f5dc0bd91c08254418f4a019e07f5afeacd0
shimanamisan <mikan.sup.all@gmail.com>
2023-08-03 06:08:21 +0900

    add: FluentMigratorを実行するコンソールアプリケーションを追加

---
+++ DataGrid.Sample.Migrator/UsersTable.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace DataGrid.Sample.Migrator
+{
+    public sealed class UsersTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("first_name").AsString()
+                .WithColumn("last_name").AsString();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
